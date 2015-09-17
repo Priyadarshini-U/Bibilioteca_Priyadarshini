d4d9e8ef782cf544c36b8a74d9658ca343341e23
priyadarshini <priyadr@thoughtworks.com>
2015-09-17 20:16:06 +0530

    movie details have respective fields

--- src/com/tw/bibilioteca/MovieDetails.java
+++ src/com/tw/bibilioteca/MovieDetails.java
@@ -1,7 +1,9 @@
 package com.tw.bibilioteca;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class MovieDetails {
     private Date yearReleased;
@@ -19,5 +21,13 @@ public class MovieDetails {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(yearReleased);
         return calendar.get(Calendar.YEAR) + " " + director + " " + rating;
+    }
+
+    public List<String> fields() {
+        List<String> fields = new ArrayList<String>();
+        fields.add("year released");
+        fields.add("director");
+        fields.add("rating");
+        return fields;
     }
 }
--- test/com/tw/bibilioteca/MovieDetailsTest.java
+++ test/com/tw/bibilioteca/MovieDetailsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MovieDetailsTest {
 
@@ -15,5 +16,32 @@ public class MovieDetailsTest {
         Double rating = 3.0;
 
         assertEquals(new MovieDetails(yearRealeased, director, rating).toString(), "2015 asd 3.0");
+    }
+    
+    @Test
+    public void shouldReturnYearReleasedAsFields() {
+        Date yearRealeased = new Date();
+        String director = "asd";
+        Double rating = 3.0;
+
+        assertTrue(new MovieDetails(yearRealeased, director, rating).fields().contains("year released"));
+    }
+
+    @Test
+    public void shouldReturnDirectorAsFields() {
+        Date yearRealeased = new Date();
+        String director = "asd";
+        Double rating = 3.0;
+
+        assertTrue(new MovieDetails(yearRealeased, director, rating).fields().contains("director"));
+    }
+
+    @Test
+    public void shouldReturnRatingAsFields() {
+        Date yearRealeased = new Date();
+        String director = "asd";
+        Double rating = 3.0;
+
+        assertTrue(new MovieDetails(yearRealeased, director, rating).fields().contains("rating"));
     }
 }
