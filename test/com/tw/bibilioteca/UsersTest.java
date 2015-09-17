af9d1cdc3fd65f3157a82c532740228de1bda97c
priyadarshini <priyadr@thoughtworks.com>
2015-09-17 22:32:53 +0530

    Users has list of users and give user information for valid token

--- test/com/tw/bibilioteca/UsersTest.java
+++ test/com/tw/bibilioteca/UsersTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class UsersTest {
-
     @Test
     public void shouldNotReturnUserInformationForInValidUserToken() {
         assertTrue(new Users().getUserInformationFromToken("11404444") == null);
