019e6babd30e1cde6d1836a8080c40c04ccb86db
priyadarshini <priyadr@thoughtworks.com>
2015-09-17 22:40:50 +0530

    login controller prompts for library number

---
+++ src/com/tw/bibilioteca/LoginController.java
@@ -0,0 +1,19 @@
+package com.tw.bibilioteca;
+
+public class LoginController implements IController {
+    private ConsoleDisplay display;
+    private Login current;
+    private BibilioticaData libraryData;
+
+    public LoginController(ConsoleDisplay display, Login current, BibilioticaData libraryData) {
+        this.current = current;
+        this.display = display;
+        this.libraryData = libraryData;
+    }
+
+    @Override
+    public IController executeAction() {
+        display.putOutput("Enter library Number");
+        return null;
+    }
+}
---
+++ test/com/tw/bibilioteca/LoginControllerTest.java
@@ -0,0 +1,32 @@
+package com.tw.bibilioteca;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class LoginControllerTest {
+
+    @Test
+    public void shouldPrintEnterLibraryNumber() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        HashMap<String, EntityDetails> bookList = new HashMap<String, EntityDetails>();
+        BookDetails bookDetails1 = new BookDetails("author", new Date());
+        String bookName1 = "name1";
+        bookList.put(bookName1, bookDetails1);
+        Catalog catalog = new Catalog(bookList, new HashMap<String, EntityDetails>());
+        ConsoleDisplay consoleDisplay = new ConsoleDisplay(System.in, new PrintStream(outContent));
+        BibilioticaData data = new BibilioticaData(catalog, null);
+        IController controller = new LoginController(consoleDisplay, new Login(), data);
+        controller.executeAction();
+
+        assertTrue(outContent.toString().contains("Enter library Number"));
+    }
+}
