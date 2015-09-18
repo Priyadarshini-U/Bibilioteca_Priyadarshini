c83a005a0df7c2388af03ab9bfe0477efc106f3b
priyadarshini <priyadr@thoughtworks.com>
2015-09-18 08:35:17 +0530

    refactored menu controller

--- src/com/tw/bibilioteca/MenuController.java
+++ src/com/tw/bibilioteca/MenuController.java
@@ -15,33 +15,36 @@ public class MenuController implements IController {
 
     public IController executeAction() {
         display.putOutput(currentModel);
-        String operation = currentModel.choose(display.getInteger());
-        String choice = operation;
-        if (!isValidOperationForCurrentUser(choice, libraryData.getSessionToken()))
+        String choice = currentModel.choose(display.getInteger());
+        final String session = libraryData.getSessionToken();
+        Catalog bookLibrary = libraryData.getBookLibrary();
+        Catalog movieLibrary = libraryData.getMoviesLibrary();
+
+        if (!isValidOperationForCurrentUser(choice, session))
             return new LoginController(display, new Login(), libraryData);
         if (choice.equals("Log in"))
             return new LoginController(display, new Login(), libraryData);
         if (choice.equals("Log out"))
-            return new MenuController(display, new Menu(UserRole.GUEST.getDisplayOperations()), new BibilioticaData(libraryData.getBookLibrary(), libraryData.getMoviesLibrary(), new GuestUser().authenticate()));
+            return new MenuController(display, new Menu(UserRole.GUEST.getDisplayOperations()), new BibilioticaData(bookLibrary, movieLibrary, new GuestUser().authenticate()));
         if (choice.equals("List Books"))
-            return new Controller(display, new EntityView(libraryData.getBookLibrary()), libraryData);
+            return new Controller(display, new EntityView(bookLibrary), libraryData);
         if (choice.equals("List Movies"))
-            return new Controller(display, new EntityView(libraryData.getMoviesLibrary()), libraryData);
+            return new Controller(display, new EntityView(movieLibrary), libraryData);
         if (choice.equals("CheckOut Book"))
-            new CatalogOperation(display, libraryData.getBookLibrary()).executeCheckout(new Users().getUserLibraryNumber(libraryData.getSessionToken()));
+            new CatalogOperation(display, bookLibrary).executeCheckout(new Users().getUserLibraryNumber(session));
         else if (choice.equals("return Book"))
-            new CatalogOperation(display, libraryData.getBookLibrary()).executeReturn(new Users().getUserLibraryNumber(libraryData.getSessionToken()));
+            new CatalogOperation(display, bookLibrary).executeReturn(new Users().getUserLibraryNumber(session));
         else if (choice.equals("CheckOut Movie"))
-            new CatalogOperation(display, libraryData.getMoviesLibrary()).executeCheckout(new Users().getUserLibraryNumber(libraryData.getSessionToken()));
+            new CatalogOperation(display, movieLibrary).executeCheckout(new Users().getUserLibraryNumber(session));
         else if (choice.equals("quit"))
             return new ExitController();
         else if (choice.equals("Book Status")) {
-            Catalog catalogDisplay = new Catalog(libraryData.getBookLibrary().getCheckedOutEntities(), libraryData.getBookLibrary().getAvailableEntities());
+            Catalog catalogDisplay = new Catalog(bookLibrary.getCheckedOutEntities(), bookLibrary.getAvailableEntities());
             return new Controller(display, new EntityView(catalogDisplay), libraryData);
         } else if (choice.equals("User Information")) {
-            Catalog catalogDisplay = new Catalog(new HashMap<String, EntityDetails>() {{
-                put("", new Users().getUserInformationFromToken(libraryData.getSessionToken()));
-            }}, libraryData.getBookLibrary().getAvailableEntities() );
+            final Catalog catalogDisplay = new Catalog(new HashMap<String, EntityDetails>() {{
+                put("", new Users().getUserInformationFromToken(session));
+            }}, bookLibrary.getAvailableEntities() );
             return new Controller(display, new EntityView(catalogDisplay), libraryData);
         } else
             display.putOutput("invalid option");
